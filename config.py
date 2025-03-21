f52a0df8347cf6dabb65737346d8c0df890d739b
BINIFLIX01 <150214066+BINIFLIX01@users.noreply.github.com>
2025-03-21 14:33:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,29 +10,29 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6812677272:AAEiQ3LNlQCoEAmih6z3T0USXqRXKZLR2CQ")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7901374466:AAHP7wBMTqbo-aiwxz7LwifsXkzpzZa1MWo")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "25336929"))
+APP_ID = int(os.environ.get("APP_ID", "23907320"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "308f4c34a6c3b90a74546d7e7e3087c2")
+API_HASH = os.environ.get("API_HASH", "c3c68636fa902388c33426958b69a886")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002077474079"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002461051729"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "5819876497"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7954762405"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://bot:HX9sva9z4s8VDOzR@cluster.pgjq3f6.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "Mongodb : mongodb+srv://surajkunaar555333:HXYCnoobDezHh6kc@cluster0.e0qih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001960439451"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002273046249"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -40,7 +40,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1928498925").split()):
+    for x in (os.environ.get("ADMINS", "0").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
