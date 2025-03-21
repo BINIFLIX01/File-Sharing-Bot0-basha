1128ac7849dc78115fdcaebdf498934e15963e3b
BINIFLIX01 <150214066+BINIFLIX01@users.noreply.github.com>
2025-03-21 14:46:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "23907320"))
 API_HASH = os.environ.get("API_HASH", "c3c68636fa902388c33426958b69a886")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002461051729"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002634930281"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "7954762405"))
